6f208defa955137a53409b48fa7a27743d141f81
Martin Unger <martin-unger2@live.at>
2024-07-31 11:06:35 +0200

    added get tasks

--- join/views.py
+++ join/views.py
@@ -106,3 +106,8 @@ class TaskView(APIView):
             task.save()
             return Response(TaskSerializer(task).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request, format=None):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
